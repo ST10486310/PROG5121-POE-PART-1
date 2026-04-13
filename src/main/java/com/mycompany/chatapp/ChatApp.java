56cc7b87ce34aeefcaf0c0faaeb29a8cf7c0c94d
ST10486310 <ST10486310@vcconnect.edu.za>
2026-04-13 08:40:12 +0200

    coded the string characters

---
+++ src/main/java/com/mycompany/chatapp/ChatApp.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chatapp;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+/**
+ *
+ * @author lab_services_student
+ */
+public class ChatApp {
+    
+    static Scanner sc = new Scanner(System.in);
+    static String savedUsername = "";
+    static String savedPassword = "";
+    static String saved = "";
+
+    public static void main(String[] args) {
+       
+        
+        
+    }
+}
